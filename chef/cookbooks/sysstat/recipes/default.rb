4f0acff9be7092477e1addeb6eb19143fa1146a9
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2015-09-20 00:37:02 +0900

    add sysstat cookbook

---
+++ chef/cookbooks/sysstat/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: sysstat
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file '/etc/defaults/sysstat' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  content 'ENABLED="true"
+'
+end
+
+template '/etc/cron.d/sysstat' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'sysstat.cron.erb'
+end
