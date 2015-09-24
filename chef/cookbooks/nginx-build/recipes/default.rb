d33932d7ed134bf61de039c3cd2da79cfa8bedc0
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2015-09-24 17:07:19 +0900

    update nginx-build to 0.5.0

--- chef/cookbooks/nginx-build/recipes/default.rb
+++ chef/cookbooks/nginx-build/recipes/default.rb
@@ -7,16 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file '/tmp/nginx-build-linux-amd64-0.4.4.tar.gz' do
-  source "https://github.com/cubicdaiya/nginx-build/releases/download/v0.4.4/nginx-build-linux-amd64-0.4.4.tar.gz"
-  not_if 'nginx-build -version | fgrep "nginx-build 0.4.4"'
+remote_file '/tmp/nginx-build-linux-amd64-0.5.0.tar.gz' do
+  source "https://github.com/cubicdaiya/nginx-build/releases/download/v0.5.0/nginx-build-linux-amd64-0.5.0.tar.gz"
+  not_if 'nginx-build -version | fgrep "nginx-build 0.5.0"'
   notifies :run, 'execute[install nginx-build]'
 end
 
 execute 'install nginx-build' do
   cwd '/tmp'
   user 'root'
-  command 'tar xvf nginx-build-linux-amd64-0.4.4.tar.gz
+  command 'tar xvf nginx-build-linux-amd64-0.5.0.tar.gz
 install nginx-build /usr/local/bin'
   action :nothing
 end
