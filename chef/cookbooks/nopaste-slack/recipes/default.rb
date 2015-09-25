9c1259dbcafb842d2c47c14fb753ced84b822ee8
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2015-09-25 13:01:53 +0900

    add nopaste-slack

---
+++ chef/cookbooks/nopaste-slack/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: nopaste-slack
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+bash "download nopaste-slack" do
+  cwd "/tmp"
+  code "gsutil cp gs://isucon5-fujiwaragumi/nopaste-slack /usr/local/bin/nopaste-slack.bin"
+  action :nothing
+  not_if "test -e /usr/local/bin/nopaste-slack"
+end
+
+file "/usr/local/bin/nopaste-slack.bin" do
+  owner "root"
+  group "root"
+  mode  "755"
+end
+
+cookbook_file "/usr/local/bin/nopaste-slack" do
+  owner "root"
+  group "root"
+  mode  "755"
+end
